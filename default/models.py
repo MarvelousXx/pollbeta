bb2f749e4a1720ea6a4bbbffc3594df4ad6f1e7d
MarvelousXx <10930190@dcsh.tp.edu.tw>
2020-10-22 12:02:56 +0800

    建構投票系統

--- default/models.py
+++ default/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Poll(models.Model):
+    subject = models.CharField(max_length=120)
+    description = models.TextField()
+    date_created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id) + ")" + self.subject
+
+class Option(models.Model):
+    poll_id = models.IntegerField()
+    title = models.CharField(max_length=120)
+    count = models.IntegerField(default=0)
