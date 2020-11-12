55c0ad5f95afa122112efcb6b9c955273897d19d
MarvelousXx <10930190@dcsh.tp.edu.tw>
2020-11-12 10:35:36 +0800

    Change 1

--- default/views.py
+++ default/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+def poll_list(req):
+    polls = models.Poll.objects.all()
+    return render(rer, 'poll_list.html', {'poll_list':polls})
